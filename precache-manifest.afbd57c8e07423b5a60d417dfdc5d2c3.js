08f62500f14a677a90e2dd17d5769b15a1b8721e
naftis <pyry.rouvila@futurice.com>
2019-02-16 23:47:37 +0900

    Updates

---
+++ precache-manifest.afbd57c8e07423b5a60d417dfdc5d2c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff39a4dabbc4e0f6ae22",
+    "url": "/sanaratkoja/static/js/runtime~main.ff39a4da.js"
+  },
+  {
+    "revision": "212093f928ba3159662c",
+    "url": "/sanaratkoja/static/js/main.212093f9.chunk.js"
+  },
+  {
+    "revision": "ec7440c4da57f9fb7528",
+    "url": "/sanaratkoja/static/js/2.ec7440c4.chunk.js"
+  },
+  {
+    "revision": "212093f928ba3159662c",
+    "url": "/sanaratkoja/static/css/main.3ccfa736.chunk.css"
+  },
+  {
+    "revision": "bac4ed47fee5a1b218902bfda3eda4be",
+    "url": "/sanaratkoja/index.html"
+  }
+];
